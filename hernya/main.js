ef50be4719d66c4ce67b9c914b14b7aa32f1eb8a
klvlsk <olesyafilka010@gmail.com>
2024-09-27 22:19:26 +0300

    task 3

--- hernya/main.js
+++
@@ -1,5 +0,0 @@
-// Это было совсем необязательно, но мне очень хотелось
-window.onload = function () {
-  let footer = document.getElementById("footer-year");
-  footer.innerHTML = new Date().getFullYear();
-};
