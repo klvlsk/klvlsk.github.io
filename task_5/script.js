6691aa43cfc463f71af2b681d4558517ffe4233e
klvlsk <olesyafilka010@gmail.com>
2024-10-12 12:07:36 +0300

    tab

--- task_5/script.js
+++ task_5/script.js
@@ -1,22 +1,19 @@
-function click(event) {
+function clickOK(event) {
     event.preventDefault();
-    let f1 = document.getElementsByName("field");
-    let f2 = document.getElementsByName("select");
+    let f = document.getElementsByName("field");
     let r = document.getElementById("result");
-    let p = document.getElementById("price");
-    if (f1[0].value.match(/^\d+$/) && f1[0].value >= 0) {
-      r.innerHTML = "Итоговая сумма: " + f1[0].value * f2[0].value;
-      p.innerHTML = "Цена (за голову): " + f2[0].value;
+    let s = document.getElementsByName("select");
+    if (f[0].value.match(/^\d+$/) && f[0].value >= 0) {
+        r.innerHTML = "Цена (за голову): " + s[0].value + "\nитоговая сумма: "
+        + f[0].value * s[0].value;
+    } else {
+        alert("Убедитесь в правильности ввода");
+        r.innerHTML = "";
     }
-    else {
-      alert("Убедитесь в правильности ввода!");
-      r.innerHTML = "";
-      p.innerHTML = "";
-    }
-  }
+}
 
-  window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", function () {
     console.log("DOM is Ready");
     let b = document.getElementById("end");
-    b.addEventListener("click", click);
-  });
+    b.addEventListener("click", clickOK);
+});
